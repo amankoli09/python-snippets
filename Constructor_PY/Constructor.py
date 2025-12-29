04da387cc4442d1f80f9a15332e27c82146d37db
Aman Koli <2025.amana@isu.ac.in>
2025-12-29 18:13:57 +0530

    Update Constructor.py

--- Constructor_PY/Constructor.py
+++ Constructor_PY/Constructor.py
@@ -1,4 +1,16 @@
-class student:
-    def __init__(self,name,roll):
-        self.name=name
-        self.roll=roll
+class Student:
+    def __init__(self, name, roll):
+        self.name = name
+        self.roll = roll
+
+    def display(self):
+        print("Name:", self.name)
+        print("Roll Number:", self.roll)
+
+
+# main program
+name = input("Enter student name: ")
+roll = int(input("Enter roll number: "))
+
+s1 = Student(name, roll)
+s1.display()
